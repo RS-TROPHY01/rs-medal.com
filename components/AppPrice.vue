7854c9349948c69fa27650c70ce360d214fee08d
NooobtimeX <u2gstudio@gmail.com>
2024-03-26 09:28:32 +0700

    rs-medal

---
+++ components/AppPrice.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="grid md:grid-cols-2">
+    <div class="text-center">
+      <h2>&#127941;ราคาเหรียญรางวัลซิงค์อัลลอย&#127941;</h2>
+      <ul>
+        <li>ขั้นต่ำ 100 เหรียญ/ผลิต 30-45 วัน</li>
+        <li>ขนาดมาตรฐาน 6.5 ซม. หนา 3.5-4 มม.</li>
+        <li>พิมพ์สีด้วย UV ได้ไม่จำกัด/ปั้นนูน 3 มิติ</li>
+        <li>สายพิมพ์ซับลิเมชั่น/ขนาด 2.5 ซม.</li>
+      </ul>
+      <div>
+        <h2>จำนวน/ราคากลาง</h2>
+        <table class="mx-auto">
+          <thead>
+            <tr>
+              <th
+                class="px-4 py-2 bg-gray-200 text-gray-700 font-bold uppercase text-sm leading-normal"
+              >
+                &#127941;จำนวนเหรียญ&#127941;
+              </th>
+              <th
+                class="px-4 py-2 bg-gray-200 text-gray-700 font-bold uppercase text-sm leading-normal"
+              >
+                <span class="text-green-600">฿</span> ราคา/เหรียญ
+                <span class="text-green-600">฿</span>
+              </th>
+              <th
+                class="px-4 py-2 bg-gray-200 text-gray-700 font-bold uppercase text-sm leading-normal"
+              >
+                <span class="text-green-600">฿</span> ค่าบล็อก
+                <span class="text-green-600">฿</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(row, rowIndex) in rows"
+              :key="rowIndex"
+              class="border-b border-gray-200"
+            >
+              <td
+                v-for="(cell, cellIndex) in row"
+                :key="cellIndex"
+                :class="{ 'text-green-500': cell === 'ฟรี' }"
+                class="px-4 py-2 text-sm leading-normal"
+              >
+                {{ cell }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div>
+      <img src="/panel/medal1.webp" height="500px" width="500px" alt="" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const rows = [
+  ["100", "150", "5,500"],
+  ["101-300", "120-130", "5,500"],
+  ["301-500", "85-95", "5,500"],
+  ["501-1,000", "60-65", "5,500"],
+  ["1,001-2,000", "50-55", "ฟรี"],
+  ["2,001-5,000	", "45-50", "ฟรี"],
+  ["5,000 ขึ้นไป", "40-45", "ฟรี"],
+];
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+const title = "RS MEDAL | โล่รางวัลคริสตัล โล่อะคริลิค คุณภาพดีแข็งแรงทนทาน";
+const description =
+  "บริษัทจัดจำหน่ายโล่รางวัลคุณภาพดีแข็งแรงทนทาน มีให้เลือกทั้งโล่รางวัลคริสตัล โล่รางวัลอะคริลิค โดยเรามีทั้งในรูปแบบหน้าร้านและออนไลน์พร้อมบริการจัดส่งทั่วประเทศไทย";
+const image = "/panel/hero_section.webp";
+useSeoMeta({
+  title: title,
+  ogTitle: title,
+  twitterTitle: title,
+  description: description,
+  ogDescription: description,
+  twitterDescription: description,
+  ogImage: image,
+  twitterImage: image,
+  keywords:
+    "RS MEDAL, โล่รางวัล, โล่คริสตัล, โล่รางวัลคริสตัล, โล่อะคริลิค, โล่รางวัลอะคริลิค",
+});
+</script>
+<template>
+  <div>
+    <AppHeroSection />
+    <div class="grid md:grid-cols-2">
+      <div>
+        <div class="text-center pt-4">
+          <div>
+            <h1>เหรียญรางวัล</h1>
+            <ul>
+              <li>&#127941;ไม่มีค่าออกแบบ&#127941;</li>
+              <li>&#127941;แนะนำในการออกแบบได้&#127941;</li>
+              <li>&#127941;สีคมชัดแข็งแรงไม่ขึ้นสนิม&#127941;</li>
+              <li>&#127941;ใช้เวลาในการผลิตตามจำนวนที่สั่ง&#127941;</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <img src="/panel/line.webp" alt="" />
+        </div>
+      </div>
+      <div class="mx-auto">
+        <img src="/panel/medal.webp" height="500px" width="500px" alt="" />
+      </div>
+    </div>
+    <AppPrice />
+    <div class="grid md:grid-cols-2">
+      <div class="p-2">
+        <img src="/panel/medal2.webp" height="500px" width="500px" alt="" />
+      </div>
+      <div class="text-center p-2">
+        <h2>&#127941;เหรียญรางวัลซิงค์อัลลอย&#127941;</h2>
+        <ul class="text-left px-8">
+          <li>&#10004;ไม่เกิดสนิม</li>
+          <li>&#10004;ทำให้ดูมีมิติได้</li>
+          <li>&#10004;ขึ้นบล๊อกด้วยโลหะ</li>
+          <li>&#10004;มีความคมชัดสูงเรียบเนียน</li>
+          <li>&#10004;ลงไม่จำกัดสีด้วย UV ไล่เฉดสีได้</li>
+        </ul>
+        <h2>&#127941;เหรียญรางวัลตะกั่ว&#127941;</h2>
+        <ul class="text-left px-8">
+          <li>&#10006;มีน้ำหนักมาก</li>
+          <li>&#10006;ขึ้นบล๊อกด้วยยาง</li>
+          <li>&#10006;ไม่มีมิติเท่างานซิงค์อัลลอย</li>
+          <li>&#10006;ลงสีได้แค่ 3-4 สี เกิดสนิมได้</li>
+        </ul>
+      </div>
+    </div>
+    <AppOurCustomer />
+  </div>
+</template>
